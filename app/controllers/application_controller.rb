cec15bb65504eedf9493cc9b0606394c5cc89f6a
Martin Villalba <martinvillalba@Martins-MacBook-Pro.local>
2025-09-22 19:07:43 +0200

    fixing admin index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 protected
 
   def after_sign_in_path_for(resource)
-    admin_dashboard_index_path
+    admin_dashboard_path
   end
 
   def current_ability
